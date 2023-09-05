bae117df414d70936f788e0bb2b727fce731fd0a
Shashank Chauhan <shashank.chauhan.ec.2018@miet.ac.in>
2023-09-05 13:57:32 +0530

    Add project files.

---
+++ Linear Search Algorithm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Search_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine(Linear_Search(A, 9, 5));
+            Console.ReadLine();
+        }
+        public static int Linear_Search(int[] Array, int N , int Key)
+        {
+            int index = 0;
+            while(index < N)
+            {
+                if (Array[index] == Key)
+                {
+                    return index;
+                }
+                index = index + 1;
+            }
+            return -1;
+        }
+    }
+}
